9e6cb1e65f8e57eba226e5c478492b0724509537
L2MT <gtrtx34@gmail.com>
2026-04-05 15:28:41 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Compte créé !");
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+}
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+}
